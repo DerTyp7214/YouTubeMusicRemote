9ce9d8424acd0cb780c65e68923d21034314aa6e
DerTyp7214 <lengwenath1@gmail.com>
2022-09-13 15:13:26 +0200

    ndk

--- app/src/main/java/de/dertyp7214/youtubemusicremote/native/ImageC.kt
+++
@@ -1,7 +0,0 @@
-package de.dertyp7214.youtubemusicremote.native
-
-import android.graphics.Bitmap
-
-internal object ImageC {
-    external fun blurBitmap(bitmap: Bitmap?, radius: Int, unpin: Int, forceLess: Int): Int
-}
